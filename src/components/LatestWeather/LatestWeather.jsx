f270962367503f9b73466ba7e26b72a081d40f0f
mr <mikkelrask@protonmail.com>
2023-04-23 19:02:18 +0200

    Latest Weather card
    A quick overview component of the last reading

--- src/components/LatestWeather/LatestWeather.jsx
+++ src/components/LatestWeather/LatestWeather.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import WeatherIcon from '../WeatherIcon'
 import { format } from 'date-fns';
 import translations from '../../services/translations.json';
+import '../../services/translate'
+import { getWindDirection } from '../../services/windDirection';
+import clearDayIcon from '../../icons/icon01d.svg';
+import fewCloudsDayIcon from '../../icons/icon02d.svg';
+import scatteredCloudsDayIcon from '../../icons/icon03d.svg';
+import brokenCloudsDayIcon from '../../icons/icon04d.svg';
+import showerRainDayIcon from '../../icons/icon09d.svg';
+import rainDayIcon from '../../icons/icon10d.svg';
+import thunderstormDayIcon from '../../icons/icon11d.svg';
+import snowDayIcon from '../../icons/icon13d.svg';
 
 const WeatherInfo = ({ label, value, unit }) => (
   <div className="flex items-center justify-between">
@@ -12,6 +21,29 @@ const WeatherInfo = ({ label, value, unit }) => (
   </div>
 );
 
+const getIcon = (iconName) => {
+  switch (iconName) {
+    case '01d':
+      return clearDayIcon;
+    case '02d':
+      return fewCloudsDayIcon;
+    case '03d':
+      return scatteredCloudsDayIcon;
+    case '04d':
+      return brokenCloudsDayIcon;
+    case '09d':
+      return showerRainDayIcon;
+    case '10d':
+      return rainDayIcon;
+    case '11d':
+      return thunderstormDayIcon;
+    case '13d':
+      return snowDayIcon;
+    default:
+      return clearDayIcon;
+  }
+};
+
 const LatestWeather = ({ data }) => {
   if (!data || data.length === 0) {
     return <div>Ingen tilgængelig vejrdata.</div>;
@@ -21,17 +53,22 @@ const LatestWeather = ({ data }) => {
   const weather = latestEntry.data;
   const { main, wind, weather: weatherArray } = weather;
   const { icon, description } = weatherArray[0];
+  const IconComponent = getIcon(icon);
 
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-4 rounded-lg shadow-md sm:w 1/1 md:w-1/2 ">
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-4 rounded-lg shadow-md w-full md:w-2/3-4 ">
       <div className="flex justify-center items-center mb-4">
-        <WeatherIcon iconName={icon} />
-        <span className="text-xl font-semibold ml-4 capitalize">{translations[description]}</span>
+      <div className="w-32 h-32">
+        <IconComponent alt={description} style={{ width: '32px', height: '32px', transform: 'scale(4)' }} className=" object-contain object-center" />
+      </div>
+        <span className="text-2xl font-semibold ml-4 capitalize">{translations[main]}</span> <br />
+        <span className="text-xl font-semibold ml-4 capitalize">{`/ ${translations[description]}`}</span>
       </div>
       <WeatherInfo label="Udendørs temperatur" value={main.temp} unit="°C" />
       <WeatherInfo label="Badevand" value={latestEntry.temperature} unit="°C" />
       <WeatherInfo label="Luftfugtighed" value={main.humidity} unit="%" />
       <WeatherInfo label="Vind" value={wind.speed} unit="m/s" />
+      <WeatherInfo label="Vindretning" value={getWindDirection(wind.deg)} unit="" />
       <WeatherInfo label="Seneste måling" value={format(new Date(latestEntry.createdAt), 'HH:mm')} unit="" />
     </div>
   );
