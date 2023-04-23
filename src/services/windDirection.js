28ac8c1fcef53c9387d349308e0476d1d55793ea
mr <mikkelrask@protonmail.com>
2023-04-23 19:11:02 +0200

    Translate openweathermap value for wind direction, to be "human readable" instead of a int/degree

---
+++ src/services/windDirection.js
@@ -0,0 +1,10 @@
+export function getWindDirection(degrees) {
+  const directions = ['N', 'NØ', 'Ø', 'SØ', 'S', 'SV', 'V', 'NV'];
+  const index = Math.round(degrees / 45) % 8;
+  let direction = directions[index];
+  if (degrees % 45 < 10 || degrees % 45 > 35) {
+    const neighbor = `${neighbor}\/`;
+    direction += directions[(index + (degrees % 45 < 10 ? 7 : 1)) % 8];
+  }
+  return direction;
+}
