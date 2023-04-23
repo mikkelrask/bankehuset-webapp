8fec89ca943ef7f652e20ec5ce9a31f7f89e4997
mr <mikkelrask@protonmail.com>
2023-04-23 19:56:00 +0200

    switched direction and "neighbor", so direction is mentioned first

--- src/services/windDirection.js
+++ src/services/windDirection.js
@@ -3,8 +3,8 @@ export function getWindDirection(degrees) {
   const index = Math.round(degrees / 45) % 8;
   let direction = directions[index];
   if (degrees % 45 < 10 || degrees % 45 > 35) {
-    const neighbor = `${neighbor}\/`;
-    direction += directions[(index + (degrees % 45 < 10 ? 7 : 1)) % 8];
+    const neighbor = directions[(index + (degrees % 45 < 10 ? 7 : 1)) % 8];
+    direction += `/${neighbor}`;
   }
   return direction;
 }
